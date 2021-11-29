6a728fbc2c849dae4c928a8f2bb304ddb70d6b7d
dukizwe <mbagapro@gmail.com>
2021-11-29 15:41:38 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+          const [mapRegion, setmapRegion] = useState({
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+          });
+          const [location, setLocation] = useState(null);
+          const [errorMsg, setErrorMsg] = useState(null);
+          (async () => {
+                    let { status } = await Location.requestForegroundPermissionsAsync();
+                    if (status !== 'granted') {
+                              setErrorMsg('Permission to access location was denied');
+                              return;
+                    }
+
+                    let location = await Location.getCurrentPositionAsync({});
+                    setLocation(location);
+                    setmapRegion(location.coords)
+          })();
+          return (
+                    <View style={styles.container}>
+                              <MapView style={styles.map} >
+                                        <Marker coordinate={mapRegion} title='Ma position' />
+                              </MapView>
+                              <StatusBar style="auto" />
+                    </View>
+          );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+          container: {
+                    flex: 1,
+                    backgroundColor: '#fff',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+          },
+          map: {
+                    width: Dimensions.get('window').width / 1.1,
+                    height: Dimensions.get('window').height / 2,
+          },
 });
